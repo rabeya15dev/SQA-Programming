1209385ed9ef5660d772994b9d362c436159e322
rabeya15dev <rabeya1012000@gmail.com>
2026-03-08 16:08:05 +0600

    added example

--- class/randomfunction.js
+++ class/randomfunction.js
@@ -3,3 +3,6 @@ function getRandomIntNumber(min,max){
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
 }
+
+
+getRandomIntNumber(2,10);
